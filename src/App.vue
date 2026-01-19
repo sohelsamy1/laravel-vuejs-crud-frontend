9a83b809aac90cf715413e55617319075ad4de7f
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-01-19 22:03:41 +0600

    feat: create axios API client with auth interceptor

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
-import LoginPage from './pages/LoginPage.vue';
-import RegistrationPage from './pages/RegistrationPage.vue';
 
 
 </script>
 
 <template>
-<!-- <RegistrationPage />
-<LoginPage /> -->
+
 <RouterView />
 </template>
 
